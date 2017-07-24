17b4cc59d35212326d11172a1db7870ea9da5e55
Naruto\Jason <29928107+Noruk75@users.noreply.github.com>
2017-07-24 21:37:38 +0200

    Added TankAIController, made it aware of the TankPlayerController, expanded the C++ classes in prep for the AimTowardsCrosshair functionality.

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+
+
+void ATankAIController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    auto ControlledTank = GetControlledTank();
+    if (!ControlledTank) {
+        UE_LOG(LogTemp, Warning, TEXT("AIController not possesing a tank"));
+    } else {
+        UE_LOG(LogTemp, Warning, TEXT("AIController possesing : %s"), *(ControlledTank->GetName()));
+    }
+
+    auto PlayerTank = GetPlayerTank();
+    if (!PlayerTank) {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerTank not found"));
+    } else {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerTank found: %s"), *(PlayerTank->GetName()));
+    }
+
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+    return Cast<ATank>(GetPawn());
+}
+
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+    auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+    if (!PlayerPawn) { return nullptr; }
+    return Cast<ATank>(PlayerPawn);
+}
---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    auto ControlledTank = GetControlledTank();
+    if (!ControlledTank) {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank"));
+    } else {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing : %s"), *(ControlledTank->GetName()));
+    }
+}
+
+// Called every frame
+void ATankPlayerController::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+    
+    //AimTowardsCrosshair
+}
+
+
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+    return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+    if (!GetControlledTank()) { return; }
+}
