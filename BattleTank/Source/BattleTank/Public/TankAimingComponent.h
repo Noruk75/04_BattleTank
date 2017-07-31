8a35a21dea830520a990cb6f2d4d47e4e8a3016a
Naruto\Jason <29928107+Noruk75@users.noreply.github.com>
2017-07-31 21:47:48 +0200

    Can now change the colour of the aiming reticule.

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -6,6 +6,15 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+// Enum for aiming state
+UENUM()
+enum class EFiringState : uint8
+{
+    Reloading,
+    Aiming,
+    Locked
+};
+
 class UTankBarrel;
 class UTankTurret;
 
@@ -26,7 +35,13 @@ public:
     void AimAt(FVector HitLocation, float LaunchSpeed);
     void MoveBarrelTowards(FVector AimDirection);
 
+protected:
+    UPROPERTY(BlueprintReadOnly, Category = "State")
+    EFiringState FiringState = EFiringState::Reloading;
+
 private:
     UTankBarrel* Barrel = nullptr;
     UTankTurret* Turret = nullptr;
+
+    
 };
